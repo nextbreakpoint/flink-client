741cb2771a345e6baa32ee28bf92e0ad0167d123
Andrea Medeghini <andrea@nextbreakpoint.com>
2020-02-14 22:57:29 +0000

    create docker network

--- pipeline/setup-docker.sh
+++ pipeline/setup-docker.sh
@@ -8,3 +8,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update -y && sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USER
+
+docker network create flink-test
